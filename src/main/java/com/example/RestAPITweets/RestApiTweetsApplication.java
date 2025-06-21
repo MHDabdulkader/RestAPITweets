a7687187e14da60230f9ed24fe0b637c4f73096f
Abdul <godholiak12345@gmail.com>
2025-06-21 18:45:25 +0600

    set up

---
+++ src/main/java/com/example/RestAPITweets/RestApiTweetsApplication.java
@@ -0,0 +1,13 @@
+package com.example.RestAPITweets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiTweetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiTweetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RestAPITweets/RestApiTweetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RestAPITweets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiTweetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
